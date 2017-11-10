a5e51acd0ec336e6a2cd75e9796649d1c84d71cb
lulu116 <sliusting970@163.com>
2017-11-10 16:24:19 +0800

    修改商品控制

--- js/addshopsubmit.js
+++ js/addshopsubmit.js
@@ -3,26 +3,27 @@
  */
 $(function(){
     //点击添加商品
-    $("#addshopsubmit").click(function () {
-      var title = $("#title").val();
-      var price = $("#price").val();
-      var color = $("#color").val();
-      var size = $("#size").val();
-      var detail = $("#detail").val();
-      var stock = $("#stock").val();
-      var img = $("#img").val();
-
-
-        $.ajax({
-            data: {title: title,price:price,color:color,size:size,detail:detail,stock:stock,img:img},
-            type: "POST",
-            url: shopUrl,
-            error: function (msg) {
-                console.log(msg);
-            },
-            success: function (data) {
-                alert(data);
-            }
+    $("#addshopsubmits").click(function () {
+         var title = $("#title").val();
+         var price = $("#price").val();
+         var color = $("#color").val();
+         var size = $("#size").val();
+         var detail = $("#detail").val();
+         var stock = $("#stock").val();
+        //alert($("#product_img").val())
+         var img = $("#product_img").val().substr($("#product_img").val().lastIndexOf("\/")+1);
+           $.ajax({
+               data: {title: title,price:price,color:color,size:size,detail:detail,stock:stock,img:img},
+               type: "POST",
+              url: shopUrl,
+              error: function (msg) {
+                  alert("商品添加失败了");
+                },
+               success: function (data) {
+                alert("商品添加成功了");
+                   window.location.reload();
+              }
+           });
         });
-    })
+
 });
