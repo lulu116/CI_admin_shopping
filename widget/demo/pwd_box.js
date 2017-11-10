f2dc2198c353e271819ba1ca4255344c7a007408
lulu116 <sliusting970@163.com>
2017-11-10 17:05:41 +0800

    修改商品控制

---
+++ widget/demo/pwd_box.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2017/11/8.
+ */
+ReactDOM.render(<input type="password" class="" placeholder="请输入密码"/>,document.getElementById("pwd_box"));
---
+++ widget/demo/user_box.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2017/11/8.
+ */
+ReactDOM.render(<input type="text" class="" placeholder="请输入账号"/>,document.getElementById("user_box"));
