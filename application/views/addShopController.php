7f1ad8b679a8579e39d6846c5c7264660b0b6a4d
lulu116 <sliusting970@163.com>
2017-11-10 15:53:19 +0800

    修改商品控制

--- application/views/addShopController.php
+++ application/views/addShopController.php
@@ -1,7 +1,10 @@
 <?php
 $this->load->view('indexHead');
 ?>
-
+<!-- 编辑器使用的==配置文件 start -->
+<script type="text/javascript" src="<?php echo base_url()?>public/plug/ue/ueditor.config.js"></script>
+<script type="text/javascript" src="<?php echo base_url()?>public/plug/ue/ueditor.all.js"></script>
+<!--  编辑器使用的==配置文件 end -->
 <!-- BEGIN PAGE -->
 <div id="main-content">
     <!-- BEGIN PAGE CONTAINER-->
@@ -88,8 +91,7 @@ $this->load->view('indexHead');
                             <div class="control-group">
                                 <label class="control-label">商品颜色</label>
                                 <div class="controls">
-                                    <input type="text" placeholder="请输入商品颜色" id="color" name="color" data-mask="999/99/999/9999/9" class="span5">
-                                    <span class="help-inline">必填</span>
+                                    <input type="color"  id="color" name="color" data-mask="999/99/999/9999/9" class="span5">
                                 </div>
                             </div>
                             <div class="control-group">
@@ -108,20 +110,27 @@ $this->load->view('indexHead');
                             <div class="control-group">
                                 <label class="control-label">商品库存</label>
                                 <div class="controls">
-                                    <input type="text" placeholder="" id="stock" name="stock" data-mask="$ 999,999,999.99" class="span5">
+                                    <input type="number" placeholder="" id="stock" name="stock" data-mask="$ 999,999,999.99" class="span5">
                                     <span class="help-inline">必填</span>
                                 </div>
                             </div>
                             <div class="control-group">
-                                <label class="control-label">商品图片</label>
+                                <label class="control-label">图片</label>
                                 <div class="controls">
-                                    <input type="file" id="img" name="img" data-mask="99-9999999" class="span5">
-                                </div>
-                            </div>
+                                    <button type="button" id="j_upload_img_btn" class="btn btn-info">多图上传</button>
 
+                                    <ul id="upload_img_wrap"  name="product_img" ></ul>
+                                    <!-- 传图片地址值用的 -->
+                                    <input type="hidden" id="product_img" name="product_img" >
+
+                                    <!-- 加载编辑器的容器：用来上传图片的，必须要，不然上传的图片会追加到上面的编辑器里面 -->
+                                    <ul id="uploadEditor">
+
+                                    </ul>
+                                </div>
                             <div class="control-group">
                                 <div class="controls">
-                                    <input type="button" id="addshopsubmit" data-mask="99-99-9999" class="span2" value="提交">
+                                    <input type="button" id="addshopsubmits" data-mask="99-99-9999" class="span2" value="提交">
                                     &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                                     <input type="reset" placeholder="" data-mask="99-99-9999" class="span2" value="重置">
                                 </div>
@@ -145,7 +154,52 @@ $this->load->view('indexFoot');
 
 
     <script>
-        var shopUrl = "<?php echo site_url('addshopsubmit/getaddshopsubmit')?>";
+        var shopUrl = "<?php echo site_url('ProductController/addProduct')?>";
+    </script>
+    <script type="text/javascript">
+        //var ue = UE.getEditor('detail'); //detail是需要加载编辑器的id
 
+
+        //==========================================================
+        // 如何单独上传图片功能
+        // 监听多图上传和上传附件组件的插入动作
+        // 这里需要实例化一个新的编辑器，防止和上面的编辑器的内容冲突
+        var uploadEditor = UE.getEditor("uploadEditor", {
+            isShow: false,
+            focus: false,
+            enableAutoSave: false,
+            autoSyncData: false,
+            autoFloatEnabled:false,
+            wordCount: false,
+            sourceEditor: null,
+            scaleEnabled:true,
+            toolbars: [["insertimage", "attachment"]]
+        });
+        uploadEditor.ready(function () {
+            uploadEditor.addListener("beforeInsertImage", _beforeInsertImage);
+        });
+
+        // 自定义按钮绑定触发多图上传和上传附件对话框事件
+        document.getElementById('j_upload_img_btn').onclick = function () {
+            var dialog = uploadEditor.getDialog("insertimage");
+            dialog.title = '多图上传';
+            dialog.render();
+            dialog.open();
+        };
+
+        // 多图上传动作
+        function _beforeInsertImage(t, result) {
+            var imageHtml = '';
+            var imgval = '';
+            for(var i in result){
+                imageHtml += '<li><img src="'+result[i].src+'" alt="'+result[i].alt+'" height="150"></li>';
+                imgval +=','+ result[i].src;
+            }
+            document.getElementById('upload_img_wrap').innerHTML = imageHtml;
+            //如果需要保存图片地址到数据，还需要把所有的图片地址作为输入值
+            //具体怎么设置看项目需求，我这里只是举个例子
+            document.getElementById('product_img').value = imgval;
+        }
     </script>
     <script src="<?php echo base_url()?>js/addshopsubmit.js"></script>
+
